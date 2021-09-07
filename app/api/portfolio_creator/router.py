9fc963a64461d2de70f1a0fb0095fefa78054959
TimBerti <T.Berti@gmx.net>
2021-09-07 12:21:45 +0200

    portfolio creator

--- app/api/portfolio_creator/router.py
+++ app/api/portfolio_creator/router.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 @router.post("/")
-def set_portfolio(risk_coefficient: float = Body(..., ge=0, le=5, embed=True), db: Session = Depends(deps.get_db)):
+def set_portfolio(risk_coefficient: float = Body(..., ge=0, le=5), db: Session = Depends(deps.get_db)):
 
     portfolio = optimal_portfolio(risk_coefficient, db)
 
