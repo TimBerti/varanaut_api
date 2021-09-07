7f661002970f1b4196e5f6110a64ed7b879aa101
TimBerti <T.Berti@gmx.net>
2021-09-07 14:05:58 +0200

    portfolio creator

--- app/main.py
+++ app/main.py
@@ -8,9 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://thawing-springs-05749.herokuapp.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
