2e828f22e42ae04ae0ad483e55dc8fab1aef9a1a
TimBerti <T.Berti@gmx.net>
2021-12-12 18:40:07 +0100

    portfolio creator rework

---
+++ app/api/portfolio_creator/main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def split_equal(z: int, n: int):
+    '''Split z in n as equal as possible parts'''
+    return [int(x + 1) if i < z % n else int(x) for i, x in enumerate([(z-z % n)/n]*n)]
+
+
+def portfolio_creator(db, risk_coefficient, n_positions):
+    '''Return portfolio with n_positions, with risk under or equal to risk_coefficient'''
+
+    sql = '''
+        SELECT ticker, cluster, implied_volatility, combined_score 
+        FROM companies_display 
+        WHERE ticker IN (
+                SELECT UNNEST(components) FROM indices 
+                WHERE ticker = 'RUA'
+            )
+        AND cluster IS NOT NULL
+        AND implied_volatility IS NOT NULL
+        AND combined_score IS NOT NULL
+        ORDER BY combined_score DESC
+    '''
+
+    df = pd.read_sql(sql, con=db.bind)
+
+    df['implied_volatility_ranker'] = df['implied_volatility'].rank(pct=True)
+
+    filtered_df = df[df['implied_volatility_ranker'] <= risk_coefficient]
+
+    clusters = filtered_df['cluster'].unique()
+
+    n_per_cluster = split_equal(n_positions, len(clusters))
+
+    positions = []
+
+    for cluster, n in zip(clusters, n_per_cluster):
+        positions += filtered_df[filtered_df['cluster']
+                                 == cluster]['ticker'].head(n).tolist()
+
+    return positions
--- app/api/portfolio_creator/router.py
+++ app/api/portfolio_creator/router.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from app.api import deps
-from app.api.portfolio_creator.src.main import optimal_portfolio
+from app.api.portfolio_creator.main import portfolio_creator
 
 
 router = APIRouter()
@@ -13,6 +13,7 @@ router = APIRouter()
 @router.post("/")
 def set_portfolio(request: Dict, db: Session = Depends(deps.get_db)):
 
-    portfolio = optimal_portfolio(request["risk_coefficient"], db)
+    portfolio = portfolio_creator(
+        db, request["risk_coefficient"], request["n_positions"])
 
     return JSONResponse(content=portfolio)
